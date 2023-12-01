ef6056a2b83dd20a57f15aa7acc4449ce3aca2c8
Marek Kohn <me@makohn.de>
2023-12-01 07:14:02 +0100

    Add solution for day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,42 @@
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    val day = "01"
+
+    val digits = mapOf(
+        "one" to "1",
+        "two" to "2",
+        "three" to "3",
+        "four" to "4",
+        "five" to "5",
+        "six" to "6",
+        "seven" to "7",
+        "eight" to "8",
+        "nine" to "9"
+    )
+
+    fun String.replace(mapping: Map<String, String>): String {
+        var str = this
+        mapping.forEach { str = str.replace(it.key, it.key.first() + it.value + it.key.last()) }
+        return str
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>) = input
+        .map { Regex("\\d").findAll(it).map { it.value } }
+        .sumOf { (it.first() + it.last()).toInt() }
+
+    fun part2(input: List<String>) = input
+        .map { it.replace(digits) }
+        .map { Regex("\\d").findAll(it).map { it.value } }
+        .sumOf { (it.first() + it.last()).toInt() }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day${day}_test")
+    val testInput2 = readInput("Day${day}_test2")
+    check(part1(testInput) == 142)
+    check(part2(testInput2) == 281)
 
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    val input = readInput("Day${day}")
+    println(part1(input))
+    println(part2(input))
 }
